54c16b825e0821a720832874c0b404c84fb382f2
gabzzola <158217055+gabzzola@users.noreply.github.com>
2024-07-18 22:40:02 -0300

    Extendendo o Taillwind
    Criando novas classes que facilitarão a escrita do projeto.

--- Projeto/tailwind.config.js
+++ Projeto/tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+       'hover-btn': '#18D760',
+       'purple-main': '#2D46B9',
+       'green-main': '#1ED760'
+      },
+      backgroundImage: theme => ({
+        'spotfy-theme': "url('src/images/bursts.svg')",
+        'spotfy-theme-mobile': "url('src/images/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    },
   },
   plugins: [],
 }
